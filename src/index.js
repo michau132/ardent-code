f1478be7abcecb4ec08f93cddd3bc55f49cbf6be
Michał Lutecki <m.lutecki@be-tse.com>
2021-03-25 22:30:47 +0100

    task

--- src/index.js
+++ src/index.js
@@ -1,2 +1,72 @@
-console.log('hi')
+import { cloneDeep } from "lodash"
 
+const root = document.querySelector('#root')
+
+export const createInvalidCode = (key, reason) => {
+  const div = document.createElement('div')
+  div.classList.add('invalid')
+  div.innerHTML = `
+    <span>Key: <b>${key}</b></span>
+    <span>${reason}</span>
+  `
+  root.appendChild(div)
+}
+
+
+export const compareObjects = (first, second) => {
+  if(
+    !first || 
+    !second || 
+    typeof first !== 'object' ||
+    typeof second !== 'object'
+    ) {
+    return null
+  }
+
+  let areObjectsSame = true
+  for (const key in first) {
+    if (Object.hasOwnProperty.call(second, key)) {
+      const firstValue = first[key];
+      const secondValue = second[key]
+
+      if(
+        firstValue && 
+        secondValue && 
+        typeof firstValue === 'object' && 
+        typeof secondValue === 'object'
+      ) {
+        areObjectsSame = !!compareObjects(firstValue, secondValue)
+        continue
+      }
+
+      const typesAreDifferent= typeof firstValue !== typeof secondValue
+      const valuesAreDifferent = firstValue !== secondValue
+
+      if(valuesAreDifferent && typesAreDifferent) {
+        createInvalidCode(key , ` different type expected: <b>${typeof firstValue}</b> get <b>${typeof secondValue}</b>`)
+        areObjectsSame = false
+        continue
+      }
+
+      if(typesAreDifferent) {
+        createInvalidCode(key , ` different type expected: <b>${typeof firstValue}</b> get <b>${typeof secondValue}</b>`)
+        areObjectsSame = false
+        continue
+      }
+
+      if(valuesAreDifferent) {
+        createInvalidCode(key , ` has two different value: <b>${firstValue.toString()}</b> and <b>${secondValue.toString()}</b>`)
+        areObjectsSame = false
+        continue
+      }
+      
+    } else {
+      createInvalidCode(key , `does not exists in object`)
+      areObjectsSame = false
+    }
+  }
+  if(areObjectsSame) {
+    return cloneDeep(second)
+  }
+  return null
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,77 @@
-it('should pass', () => {
-  expect(true).toBeTruthy()
+import { compareObjects } from "./index";
+
+
+describe('testing compareObjects func', () => {
+  afterEach(() => {
+    document.querySelector('#root').innerHTML= ''
+  });
+  it('should be defined', () => {
+    expect(compareObjects).toBeDefined()
+  });
+  
+  it('should return null, coz of passing null', () => {
+    const result = compareObjects(null, null)
+    expect(result).toBeNull()
+  });
+
+  it('should return null, coz of uncompatible types', () => {
+    const obj1 = {
+      a: 12,
+      b: "1"
+    }
+    const obj2 = {
+      a: 12,
+      b: 1
+    }
+    const result = compareObjects(obj1, obj2)
+    expect(result).toBeNull()
+    expect(document.querySelectorAll('.invalid').length).toEqual(1)
+  });
+
+  it('should fail and return null because in array we have different order', () => {
+    const obj1 = {
+      a: {
+        b: 12,
+        c: ['yes', 123]
+      }
+    }
+    const obj2 = {
+      a: {
+        b: 12,
+        c: [123, 'yes']
+      }
+    }
+    const result = compareObjects(obj1, obj2)
+    expect(result).toBeNull()
+    expect(document.querySelectorAll('.invalid').length).toEqual(2)
+  });
+  
+  
+  it('should return copy of input object when we have nested object and array', () => {
+    const obj1 = {
+      a: {
+        b: 12,
+        c: ['yes', '123'],
+        d: {
+          e: {
+            f: 'ok'
+          }
+        }
+      },
+    }
+    const obj2 = {
+      a: {
+        b: 12,
+        c: ['yes', '123'],
+        d: {
+          e: {
+            f: 'ok'
+          }
+        }
+      }
+    }
+    const result = compareObjects(obj1, obj2)
+    expect(result).toEqual(obj1)
+    expect(document.querySelectorAll('.invalid').length).toEqual(0)
+  });
 });
